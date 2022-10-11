14da96402f88f51edb50ac9101c5732ca0cf309e
LeonardoSantos97 <leonardopsantos97@gmail.com>
2022-10-10 23:53:39 -0300

    Implemented pagination in TopicosController

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,14 +13,14 @@ import br.com.alura.forum.modelo.Topico;
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
 	// Desta forma o Spring Data monta as querys automaticamente
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
 	
 	/* Caso não queira usar o padrão do Spring Data JPA 
 	 * pode criar o método passando a query com o @Query
 	 * e passar a assinatura @Param
 	 */
-	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
-	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso")String nomeCurso);
+//	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso")String nomeCurso);
 
 }
